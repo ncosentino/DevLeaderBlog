7592b6fcf7ac433aac192d5581d2bbc864a8b3e9
Steven Giesel <stgiesel35@gmail.com>
2023-09-23 10:59:04 +0200

    Remove dependency

--- src/LinkDotNet.Blog.Web/DatabaseHealthCheck.cs
+++ src/LinkDotNet.Blog.Web/DatabaseHealthCheck.cs
@@ -6,17 +6,10 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace LinkDotNet.Blog.Web;
 
-public class DatabaseHealthCheck : IHealthCheck
+public class DatabaseHealthCheck(IRepository<BlogPost> repository) : IHealthCheck
 {
-    private readonly IRepository<BlogPost> repository;
-
-    public DatabaseHealthCheck(IRepository<BlogPost> repository)
-    {
-        this.repository = repository;
-    }
-
     public Task<HealthCheckResult> CheckHealthAsync(
         HealthCheckContext context,
-        CancellationToken cancellationToken =  default) =>
+        CancellationToken cancellationToken = default) =>
         repository.PerformHealthCheckAsync().AsTask();
 }
